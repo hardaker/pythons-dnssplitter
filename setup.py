2c89e25320e32afb76b18b688f03b7604bbb05c0
Wes Hardaker <opensource@hardakers.net>
2020-07-17 13:52:08 -0700

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dnssplitter",
-    version="1.0.0",
+    version="1.1.0",
     author="Wes Hardaker",
     author_email="opensource@hardakers.net",
     description="A fast python implementation of breaking down DNS domains into parts.",
